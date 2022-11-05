93e139a03a3f4c45c6448011d6b49c9ae23f6ac3
kojix2 <2xijok@gmail.com>
2022-11-05 21:35:57 +0900

    update capi

--- spec/onnxruntime_spec.cr
+++ spec/onnxruntime_spec.cr
@@ -3,9 +3,13 @@ require "./spec_helper"
 describe OnnxRuntime do
   # TODO: Write tests
 
-  it "works" do
+  it "has a version number" do
+    (OnnxRuntime::VERSION).should_not be_nil
+  end
+
+  it "version" do
     base = OnnxRuntime::LibOnnxRuntime.OrtGetApiBase
-    p String.new(base.value.get_version_string.call)
-    false.should eq("version")
+    v = String.new(base.value.get_version_string.call)
+    v.should be_a(String)
   end
 end
