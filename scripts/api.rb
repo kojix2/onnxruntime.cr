6122ab5adf0130db46ed83285779cbc3458cea4a
kojix2 <2xijok@gmail.com>
2025-01-18 08:04:15 +0000

    Update version of files

--- scripts/api.rb
+++ scripts/api.rb
@@ -1,7 +1,7 @@
 require "json"
 require 'active_support/core_ext/string/inflections'
 
-m = JSON.parse(File.read("capi.json"))
+m = JSON.parse(File.read(File.join(__dir__, "capi.json")))
 a = m.find_all {|i| i["name"] == "OrtApi"}.last
 a["fields"].each do |i|
   puts i["name"].underscore
