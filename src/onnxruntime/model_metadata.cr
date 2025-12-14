4056eb885339820e4e70369399e95b3de8209e0e
kojix2 <2xijok@gmail.com>
2025-12-14 22:26:56 +0900

    fix: update binding methods to use unsafe pointers and improve metadata retrieval

--- src/onnxruntime/model_metadata.cr
+++ src/onnxruntime/model_metadata.cr
@@ -20,22 +20,34 @@ module OnnxRuntime
 
       begin
         # Get producer name
-        producer_name = get_string_from_metadata(api, metadata, "producerName", session)
+        producer_name = get_metadata_string(api, metadata, session.allocator, session) do |md, alloc, ptr|
+          api.model_metadata_get_producer_name.call(md, alloc, ptr)
+        end
 
         # Get graph name
-        graph_name = get_string_from_metadata(api, metadata, "graphName", session)
+        graph_name = get_metadata_string(api, metadata, session.allocator, session) do |md, alloc, ptr|
+          api.model_metadata_get_graph_name.call(md, alloc, ptr)
+        end
 
         # Get domain
-        domain = get_string_from_metadata(api, metadata, "domain", session)
+        domain = get_metadata_string(api, metadata, session.allocator, session) do |md, alloc, ptr|
+          api.model_metadata_get_domain.call(md, alloc, ptr)
+        end
 
         # Get description
-        description = get_string_from_metadata(api, metadata, "description", session)
+        description = get_metadata_string(api, metadata, session.allocator, session) do |md, alloc, ptr|
+          api.model_metadata_get_description.call(md, alloc, ptr)
+        end
 
         # Get graph description
-        graph_description = get_string_from_metadata(api, metadata, "graphDescription", session)
+        graph_description = get_metadata_string(api, metadata, session.allocator, session) do |md, alloc, ptr|
+          api.model_metadata_get_graph_description.call(md, alloc, ptr)
+        end
 
         # Get version
-        version = get_int64_from_metadata(api, metadata, "version", session)
+        version = 0_i64
+        status = api.model_metadata_get_version.call(metadata, pointerof(version))
+        session.check_status(status)
 
         # Get custom metadata map
         custom_metadata_map = get_custom_metadata_map(api, metadata, session)
@@ -50,16 +62,31 @@ module OnnxRuntime
     private def initialize(@producer_name, @graph_name, @domain, @description, @graph_description, @version, @custom_metadata_map)
     end
 
+    # Helper method to get metadata string using API function
+    private def self.get_metadata_string(api, metadata, allocator, session, &)
+      value_ptr = Pointer(Pointer(UInt8)).malloc(1)
+      status = yield metadata, allocator, value_ptr
+
+      # If the status indicates an error, return empty string
+      if !status.null?
+        api.release_status.call(status)
+        return ""
+      end
+
+      value = value_ptr.value.null? ? "" : String.new(value_ptr.value)
+      value
+    end
+
     # Helper method to get string from metadata
     private def self.get_string_from_metadata(api, metadata, key, session)
       allocator = session.allocator
-      value_ptr = Pointer(Pointer(UInt8)).null
+      value_ptr = Pointer(Pointer(UInt8)).malloc(1)
 
-      status = api.model_metadata_get_custom_metadata_value_by_key.call(
+      status = api.model_metadata_lookup_custom_metadata_map.call(
         metadata,
-        key,
+        key.to_unsafe,
         allocator,
-        pointerof(value_ptr)
+        value_ptr
       )
 
       # If key doesn't exist, return empty string
@@ -68,7 +95,7 @@ module OnnxRuntime
         return ""
       end
 
-      value = value_ptr.null? ? "" : String.new(value_ptr)
+      value = value_ptr.value.null? ? "" : String.new(value_ptr.value)
       value
     end
 
@@ -84,13 +111,13 @@ module OnnxRuntime
     private def self.get_custom_metadata_map(api, metadata, session)
       allocator = session.allocator
       keys_count = 0_u64
-      keys = Pointer(Pointer(UInt8)).null
+      keys = Pointer(Pointer(Pointer(UInt8))).malloc(1)
 
       # Get custom metadata keys count
-      status = api.model_metadata_get_custom_metadata_keys.call(
+      status = api.model_metadata_get_custom_metadata_map_keys.call(
         metadata,
         allocator,
-        pointerof(keys),
+        keys,
         pointerof(keys_count)
       )
       session.check_status(status)
@@ -99,10 +126,13 @@ module OnnxRuntime
       custom_metadata = {} of String => String
 
       # Get custom metadata values
-      keys_count.times do |i|
-        key = String.new(keys[i])
-        value = get_string_from_metadata(api, metadata, key, session)
-        custom_metadata[key] = value
+      if keys_count > 0
+        keys_array = keys.value
+        keys_count.times do |i|
+          key = String.new(keys_array[i])
+          value = get_string_from_metadata(api, metadata, key, session)
+          custom_metadata[key] = value
+        end
       end
 
       custom_metadata
